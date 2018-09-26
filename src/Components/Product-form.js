0fe8c7f8fc03b937ab288d64370dc836a2b6525d
Richard Tom <tomrichard14@gmail.com>
2018-09-26 09:29:29 -0700

    added select for subtypes

--- src/Components/Product-form.js
+++ src/Components/Product-form.js
@@ -3,6 +3,13 @@ import { connect } from 'react-redux';
 import { addProduct } from '../actions/product';
 
 class ProductForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      type: ['Subtypes']
+    };
+  }
   onSubmit(e) {
     e.preventDefault();
     const data = {
@@ -10,23 +17,46 @@ class ProductForm extends React.Component {
       brand: e.target.brand.value,
       model: e.target.model.value,
       nickname: e.target.nickname.value,
-      comment: e.target.comment.value
+      comment: e.target.comment.value,
+      subtype: e.target.subtype.value === 'subtypes' ? '' : e.target.subtype.value 
     };
     this.props.dispatch(addProduct(data));
   }
+  
+  handleProductChange(e) {
+    if (e.target.value ==='razor') {
+      this.setState({type:['Subtypes','Double Edge', 'Straight Razor', 'Shavette', 'Cartidge', 'Single Edge']});
+    }
+    else if (e.target.value === 'brush') {
+      this.setState({type:['Subtypes','Badger', 'Boar', 'Horse', 'Synthetic']});
+    }
+    else if (e.target.value === 'lather') {
+      this.setState({type:['Subtypes','Cream', 'Soap', 'Oil']});
+    }
+    else {
+      this.setState({type:['Subtypes']});
+    }
+  }
 
   render() {
+    const typeList = this.state.type.map((type,index) => {
+      return (
+        <option value={type.toLowerCase()} key={index}>{type}</option>
+      );
+    });
+
     return (
       <form onSubmit={e => this.onSubmit(e)}>
-        <select name='productType'>
-          <option>Type</option>
+        <select onChange={e => this.handleProductChange(e)} name='productType' id='productType'>
+          <option value=''>Product Type</option>
           <option value='razor'>Razor</option>
-          <option value='blade'>Blade</option>
+          <option value='blade'>Blade</option> 
           <option value='brush'>Brush</option>
           <option value='lather'>Lather</option>
           <option value='aftershave'>Aftershave</option>
           <option value='additonalcare'>Additional Care</option>
         </select>
+        <select id='subtype' name='subtype'>{typeList}</select>
         <input name='brand' placeholder='brand'/>
         <input name='model' placeholder='model'/>
         <input name='nickname' placeholder='nickname'/>
