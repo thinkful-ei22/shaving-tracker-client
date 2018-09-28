53310734d988a898df3f776f5c59a27d15860a29
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-09-28 15:59:11 -0600

    move the Provider component inside App.jsx so the tests can find the store

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import './styles/App.css';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import './styles/App.css';
+import store from '../store';
 import LandingPage from './LandingPage';
 import MyCollection from './My-Collection';
 import ShaveHistory from './Shave-history';
 import NavBar from './Navbar';
 
 const App = () => (
-  <Router>
-    <div>
-      <NavBar />
-      <Route exact path="/" component={LandingPage} />
-      <Route exact path="/mycollection" component={MyCollection} />
-      <Route exact path="/shaves" component={ShaveHistory} />
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/mycollection" component={MyCollection} />
+        <Route exact path="/shaves" component={ShaveHistory} />
+      </div>
+    </Router>
+  </Provider>
 );
 
 
