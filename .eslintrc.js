1cdb29a366275a89126a39201ddc700cca8ba286
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-10-03 12:35:51 -0600

    remove eslint dev-dependencies yet again

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
 module.exports = {
-    "extends": "airbnb",
-    "rules": {
-        "linebreak-style": "off",
-        "jsx-a11y/label-has-for": "off",
-        "jsx-a11y/label-has-associated-control": [ 2, {
-            "labelComponents": ["label"],
-            "labelAttributes": ["htmlFor"],
-            "controlComponents": ["input"]
-          }]
-    }
+    "extends": "react-app",
 };
