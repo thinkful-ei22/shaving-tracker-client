2c70bf982d2eee63354ede5eda8041dda40ff5be
Jonathan Kim <jonkim204@gmail.com>
2018-10-05 09:16:21 -0700

    fixing travis issue

--- src/components/My-Collection.jsx
+++ src/components/My-Collection.jsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import {
   Tab, Tabs, TabList, TabPanel,
 } from 'react-tabs';
-import { Link } from 'react-router-dom';
 import ProductForm from './Product-form';
 import 'react-tabs/style/react-tabs.css';
 import requiresLogin from './requires-login';
--- src/components/Shave-history.jsx
+++ src/components/Shave-history.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ShaveForm from './Shave-form';
 import { PropTypes } from 'prop-types';
-import { Link } from 'react-router-dom';
 import requiresLogin from './requires-login';
 import './styles/shave-history.css';
 import {
