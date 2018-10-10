b96d31f4dffd12fbf99aa16b9185f8c75abffa9d
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-10-10 11:10:38 -0600

    display total usage of each item in MyCollection

--- src/components/Collection-card.jsx
+++ src/components/Collection-card.jsx
@@ -3,31 +3,26 @@ import PropTypes from 'prop-types';
 
 const CollectionCard = (props) => {
   const {
-    brand, model, subtype, nickname, comment, imageUrl,
+    brand, model, subtype, nickname, comment, imageUrl, totalUsage,
   } = props;
   return (
     <div className="collection-item">
       <img src={imageUrl} alt="" />
       <h3>{brand}</h3>
       <p>
-        <span className="collection-item-type">Model:</span>
-        {' '}
-        {model}
+        <span className="collection-item-type">Model:</span> {model}
       </p>
       <p>
-        <span className="collection-item-type">Subtype:</span>
-        {' '}
-        {subtype}
+        <span className="collection-item-type">Subtype:</span> {subtype}
       </p>
       <p>
-        <span className="collection-item-type">Nickname:</span>
-        {' '}
-        {nickname}
+        <span className="collection-item-type">Nickname:</span> {nickname}
       </p>
       <p>
-        <span className="collection-item-type">Comment:</span>
-        {' '}
-        {comment}
+        <span className="collection-item-type">Comment:</span> {comment}
+      </p>
+      <p>
+        <span className="collection-item-type">Uses:</span> {totalUsage}
       </p>
     </div>
   );
