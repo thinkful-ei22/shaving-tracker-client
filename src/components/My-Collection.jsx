7405884cfff56e2cb09a9b14f423072e45ce48d3
dghero <devingreyhero@gmail.com>
2018-10-10 13:32:31 -0700

    Fix safety concern for Collection Footer, minor footer stylings

--- src/components/My-Collection.jsx
+++ src/components/My-Collection.jsx
@@ -104,7 +104,7 @@ class MyCollection extends React.Component {
             {collections}
           </div>
         </Tabs>
-        <div className="collection-footer">Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        <div className="collection-footer">Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
       </div>
     );
   }
