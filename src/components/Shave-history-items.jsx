5d757d50e347cf2d74aa6b699a210f394bc443db
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-10-12 11:25:35 -0600

    remove console.log of image url from ShaveHistoryItems

--- src/components/Shave-history-items.jsx
+++ src/components/Shave-history-items.jsx
@@ -78,7 +78,6 @@ export class ShaveHistoryItems extends React.Component {
         ]
         : '';
 
-      console.log(sortedShaveHist[i].imageUrl);
       const imgUrl = sortedShaveHist[i].imageUrl
         ? sortedShaveHist[i].imageUrl
         : 'https://i.imgur.com/QvDFh9r.png'
