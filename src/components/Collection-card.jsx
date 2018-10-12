6434e98654abcefef3f2290b634066140ee3ca8c
dghero <devingreyhero@gmail.com>
2018-10-12 10:00:53 -0700

    Mimic Shave card's info organization

--- src/components/Collection-card.jsx
+++ src/components/Collection-card.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './styles/collection-card.css';
+
 const CollectionCard = (props) => {
   const {
     brand, model, subtype, nickname, comment, imageUrl, totalUsage,
@@ -10,25 +12,27 @@ const CollectionCard = (props) => {
 
   return (
     <div className="collection-item">
+      <h3>{nickname}</h3>
       <div className="image-div-normalizer">
         <img src={usedImageUrl} alt="" />
       </div>
-      <h3>{nickname}</h3>
-      <p>
-        <span className="collection-item-type">Brand:</span> {brand}
-      </p>
-      <p>
-        <span className="collection-item-type">Model:</span> {model}
-      </p>
-      <p>
-        <span className="collection-item-type">Subtype:</span> {subtype}
-      </p>
-      <p>
-        <span className="collection-item-type">Comment:</span> {comment}
-      </p>
-      <p>
-        <span className="collection-item-type">Uses:</span> {totalUsage}
-      </p>
+      
+      <div className="collection-item-info">
+        <span className="collection-item-info--label">Brand:</span> 
+        <span className="collection-item-type--value">{brand}</span> 
+      
+        <span className="collection-item-type--label">Model:</span>
+        <span className="collection-item-type--value">{model}</span> 
+
+        <span className="collection-item-type--label">Subtype:</span>
+        <span className="collection-item-type--value">{subtype}</span>
+
+        <span className="collection-item-type--label">Comment:</span>
+        <span className="collection-item-type--value">{comment}</span> 
+        
+        <span className="collection-item-type--label">Uses:</span>
+        <span className="collection-item-type--value">{totalUsage}</span> 
+      </div>
     </div>
   );
 };
